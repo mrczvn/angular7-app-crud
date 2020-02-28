849da624efde959dc3a91680823ca8565c8865ce
Marcos Vinicius <marvn.ants1@gmail.com>
2020-02-27 23:38:03 -0300

    feat: criado e definindo service do path users

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+// add imports 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const ApiRoutes = {
+  login: 'login',
+  signup: 'signup',
+  users: 'users'
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private UriApi: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  public login(user: any) {
+
+    let uri = `${this.UriApi}/${ApiRoutes.login}`;
+
+    return this.http.post(uri, JSON.stringify(user), this.loadHeaders());
+
+  }
+
+  public getUsers(token: string = '') {
+
+    let uri = `${this.UriApi}/${ApiRoutes.users}`;
+    return this.http.get<Array<any>>(uri, this.loadHeaders(token));
+  }
+
+  private loadHeaders(token: string = '') {
+
+    let headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': `${token}`
+    });
+
+    return { headers };
+  }
+}
