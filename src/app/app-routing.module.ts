c30b2de97488ba16014122ce757f3a610027d2a2
Marcos Vinicius <marvn.ants1@gmail.com>
2020-02-27 23:46:28 -0300

    feat: adicionando os paths ao modulo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// add imports
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { AuthGuardService } from './guard/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'users',
+    canActivate: [AuthGuardService],
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
